aeee5ddb7f4de49420d664fa967b668be48f2ed7
luan1221 <luan.ssenaa@gmail.com>
2020-08-27 15:22:28 -0300

    botao e funcionalidade do formulario para cadastro de alunos

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   aluno: Aluno = {nome: "", cpf: "", email: "", github: ""};
+  aluno: Aluno = { nome: "", cpf: "", email: ""};
+  alunoService = new AlunoService();
+
+  gravar(a: Aluno): void {
+    this.alunoService.gravar(a);
+    this.aluno = {nome: "", cpf: "", email: ""};
+  }
 }
 
-export class Aluno {
-  nome: string;
-  cpf: string;
-  email: string;
-  github: string;
-}
