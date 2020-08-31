fcfb56f6d8469ed82f50ae883dcc0f977fb2517b
luan1221 <luan.ssenaa@gmail.com>
2020-08-31 19:19:10 -0300

    metas de gerencia de projetos e testes adicionadas ao conjunto de metas do aluno

--- ta-gui/src/app/alunos.component.ts
+++ ta-gui/src/app/alunos.component.ts
@@ -2,44 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { Aluno } from '../../../common/aluno';
 import { AlunoService } from './aluno.service';
 
-  @Component({
-   selector: 'app-root',
-   templateUrl: './alunos.component.html',
-   styleUrls: ['./alunos.component.css']
- })
- export class AlunosComponent implements OnInit {
+@Component({
+  selector: 'app-root',
+  templateUrl: './alunos.component.html',
+  styleUrls: ['./alunos.component.css']
+})
+export class AlunosComponent implements OnInit {
 
-    aluno: Aluno = new Aluno();
-    alunos: Aluno[] = [];
-    cpfduplicado: boolean = false;
+  aluno: Aluno = new Aluno();
+  alunos: Aluno[] = [];
+  cpfduplicado: boolean = false;
 
-    constructor(private alunoService: AlunoService) {}
+  constructor(private alunoService: AlunoService) { }
 
-     criarAluno(a: Aluno): void {
-       this.alunoService.criar(a)
-              .subscribe(
-                ar => {
-                  if (ar) {
-                    this.alunos.push(ar);
-                    this.aluno = new Aluno();
-                  } else {
-                    this.cpfduplicado = true;
-                  } 
-                },
-                msg => { alert(msg.message); }
-              );
-    } 
-
-    onMove(): void {
-       this.cpfduplicado = false;
-    }
-
-     ngOnInit(): void {
-       this.alunoService.getAlunos()
-             .subscribe(
-               as => { this.alunos = as; },
-               msg => { alert(msg.message); }
-              );
-     }
-
+  criarAluno(a: Aluno): void {
+    this.alunoService.criar(a)
+      .subscribe(
+        ar => {
+          if (ar) {
+            this.alunos.push(ar);
+            this.aluno = new Aluno();
+          } else {
+            this.cpfduplicado = true;
+          }
+        },
+        msg => { alert(msg.message); }
+      );
+      alert("Ja executei o criar e o subscribe!");
   }
+
+  onMove(): void {
+    this.cpfduplicado = false;
+  }
+
+  ngOnInit(): void {
+    this.alunoService.getAlunos()
+      .subscribe(
+        as => { this.alunos = as; },
+        msg => { alert(msg.message); }
+      );
+  }
+
+}
